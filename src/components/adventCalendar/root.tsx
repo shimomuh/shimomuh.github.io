3e483e1c8ace75af061ee36fb071d0507b01f08a
shimomuh <shimomuh0501@gmail.com>
2019-06-26 02:30:02 +0900

    汎用っぽい構成を意識してカレンダーを作った

---
+++ src/components/adventCalendar/root.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Calendar } from './calendar';
+
+const AdventCalendar: React.FC = () => {
+  return (
+    <div className='advent-calendar'>
+      <Calendar />
+    </div>
+  );
+}
+
+export default AdventCalendar;
